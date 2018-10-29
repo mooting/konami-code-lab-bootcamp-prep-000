2d5b8e08bba36e42b5b0be171ff51e7aba9dfdde
Justin Davis <justin_w_davis@yahoo.com>
2018-10-29 06:25:16 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -10,10 +10,21 @@ const codes = [
   "b",
   "a"
 ];
-
+var codeIndex = 0
 function init() {
   const body = document.querySelector( 'body' )
+  
   body.addEventListener( 'keydown', event => {
-    console.log( event.key )
+    if( codes[codeIndex] === event.code ){
+
+      if( codeIndex === codes.length - 1 ){
+        codeIndex = 0
+        alert('Hurray!')
+      }else{
+        codeIndex++
+      }
+    }else{
+      codeIndex = 0
+    }
   })
 }
